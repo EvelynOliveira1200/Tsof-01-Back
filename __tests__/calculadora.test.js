9b5d876223466eb07da9fa80a9cf0ad50e2b83e2
EvelynOliveira1200 <evelynoliveira.tds1.senai@gmail.com>
2025-08-26 14:33:05 -0300

    feat: creat subitrair test

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,4 +1,5 @@
 import { somar } from "../src/calculadora.js";
+import { subtrair } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
     test("Deve somar dois números corretamente", () => {
@@ -12,5 +13,17 @@ describe("Calculadora", () => {
 
         //Assert (Verificar)
         expect(resultado).toBe(12);
+    });
+
+    test("Deve subtrair dois números corretamente", () => {
+        //Arrange (Preparar)
+        const a = 10;
+        const b = 4;
+
+        //Act (Executar)
+        const resultado = subtrair(a, b);
+
+        //Assert (Verificar)
+        expect(resultado).toBe(6);
     });
 })
